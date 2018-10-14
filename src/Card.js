b43b49c7c30abc2dd454499ac2fec7596b403232
a-tibrewal <aditya7bgp@gmail.com>
2018-10-14 23:02:16 +0530

    handling scroll into view

--- src/Card.js
+++ src/Card.js
@@ -6,10 +6,18 @@ class Card extends Component {
    this.state = {};   
  }
 
+ componentDidUpdate(){
+     if( this.props.selected){
+        this.currentDiv.scrollIntoView();
+     }
+ }
+
+
  render(){
      let { address, selected } = this.props;
+    //  console.log( this );
      return(
-        <div className={ "card" + (selected ? " selected" : "") } tabIndex="1">
+        <div className={ "card" + (selected ? " selected" : "") } tabIndex="1" ref={ref => this.currentDiv = ref} >
             <div className="card__id" >{address.id }</div>
             <div className="card__name" >{address.name }</div>
             <div className="card__address" >{address.address }</div>
