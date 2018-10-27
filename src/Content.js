06d65bf7f181637bae534f567639e3deb5b34729
a-tibrewal <aditya7bgp@gmail.com>
2018-10-27 15:46:25 +0530

    remove dangerously set inner html

--- src/Content.js
+++ src/Content.js
@@ -9,11 +9,39 @@ class Content extends Component {
 
  render(){
      let { content, query } = this.props;
-     if( query ){
-        content = content.replace( new RegExp( query, 'gi'), '<highlight>$&</highlight>' ); 
-     }
-     return (
-         <div dangerouslySetInnerHTML={{ __html: content }} >
+
+    // console.log( typeof( content ));
+    //  if( query ){
+    //     content = content.replace( new RegExp( query, 'gi'), '<span>$&</span>' ); 
+    //  }
+
+    
+    // all_content = '';
+    
+    // for(let i = 0; i < content.length; i +  ){
+    //     if( content[i] == query ){
+    //         all_content = (<span> content[i] <span>)
+    //     } else {
+    //         all_content = content[i];
+    //     }
+    // }
+
+    const segregate = function(content){
+
+        if( content.indexOf(query) === -1 ){
+            return content;
+        } else {
+            var startIndex = content.indexOf(query);
+            var queryLength = query.length;
+            return (<span> {content.substr(0, startIndex)} <span className="highlight">{query}</span>{ content.substr(startIndex + queryLength)}</span>);
+        }
+    }
+
+   
+
+    return (
+         <div >
+             { segregate(content)}
         </div>
      )
     }
